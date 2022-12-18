948766ba663f1354235c77bb9a49e1a315b04ab8
KakuYuki <yuki515396@yahoo.com.tw>
2022-12-18 11:51:07 +0800

    初始化

---
+++ Japanese-Test-System/src/main/java/com/Japanese/Project/Japanese/Test/System/JapaneseTestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Japanese.Project.Japanese.Test.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseTestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseTestSystemApplication.class, args);
+	}
+
+}
---
+++ Japanese-Test-System/src/test/java/com/Japanese/Project/Japanese/Test/System/JapaneseTestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Japanese.Project.Japanese.Test.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseTestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
